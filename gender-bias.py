6aa6ef85bfc88366be6745b99451ec23d6e49e6a
Marcelo Prates <marceloorp@gmail.com>
2017-12-13 12:58:45 -0200

    Finished Python scripts to 1) translate jobs into different languages 2) organize everything in .csv tables and 3) translate sentences back into english, extracting their predicted gender. The results of this extraction are saved in the table job-genders.csv

---
+++ gender-bias.py
@@ -0,0 +1,79 @@
+
+from googletrans import Translator
+
+translator = Translator()
+
+def get_gender(occupation,language):
+
+	translation = ''
+
+	if(language == 'Malay'):
+		translation = translator.translate('dia adalah %s' % occupation, src=language, dest='en').text
+	elif(language == 'Estonian'):
+		translation = translator.translate('ta on %s' % occupation, src=language, dest='en').text
+	elif(language == 'Finnish'):
+		translation = translator.translate('hän on %s' % occupation, src=language, dest='en').text
+	elif(language == 'Hungarian'):
+		translation = translator.translate('ő %s' % occupation, src=language, dest='en').text
+	elif(language == 'Armenian'):
+		translation = translator.translate('նա %s է' % occupation, src=language, dest='en').text
+	#else if(language == 'Bengali'):
+	#	translation = translator.translate('dia adalah %s' % occupation,dest='en').text
+	#else if(language == 'Nepali'):
+	#	translation = translator.translate('dia adalah %s' % occupation,dest='en').text
+	#elif(language == 'Japanese'):
+	#	translation = translator.translate('あいつ %s' % occupation, src=language, dest='en').text
+	#elif(language == 'Korean'):
+	#	translation = translator.translate('그는 %s' % occupation, src=language, dest='en').text
+	elif(language == 'Turkish'):
+		translation = translator.translate('o bir %s' % occupation, src=language, dest='en').text
+	elif(language == 'Yoruba'):
+		translation = translator.translate('o jẹ %s' % occupation, src=language, dest='en').text
+	elif(language == 'Basque'):
+		translation = translator.translate('%s da' % occupation, src=language, dest='en').text
+	elif(language == 'Swahili'):
+		translation = translator.translate('yeye ni %s' % occupation, src=language, dest='en').text
+	#elif(language == 'Chinese'):
+	#	translation = translator.translate('tā %s' % occupation, src='zh-cn', dest='en').text
+
+	translation = translation.lower()
+
+	if(translation[0:4].find("she") != -1 or translation[0:4].find("she's") != -1):
+		return 'Female'
+	elif(translation[0:4].find("he") != -1 or translation[0:4].find("he's") != -1):
+		return 'Male'
+	else:
+		return '?'
+
+with open('jobs.csv','r') as jobs:
+	with open('job-genders.csv','w') as output:
+
+		languages = list(map(lambda x: x.rstrip(), jobs.readline().split(';')[2:]))
+		discarded_languages = ['Bengali','Nepali','Japanese','Korean','Chinese']
+
+		# Write header
+		output.write('category')
+		output.write(';English')
+		output.write('\n')
+		for language in languages:
+			if language not in discarded_languages:
+				output.write(';%s' % language)
+	
+		# For each job, translate into each language and fetch the corresponding gender
+		for line in jobs:
+			category = line.split(';')[0]
+			english_name = line.split(';')[1]
+			foreign_names = list(map(lambda x: x.rstrip(), line.split(';')[2:]))
+
+			output.write('%s' % category)
+			output.write(';%s' % english_name)
+	
+			for (language,occupation) in zip(languages,foreign_names):
+				if(language not in discarded_languages):
+					try:
+						gender = get_gender(occupation,language)
+						output.write(';%s' % gender)
+					except ValueError:
+						output.write(';?')
+
+			output.write('\n')
