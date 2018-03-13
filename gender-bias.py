dc2d697bdfea38d8ef12c3692c93dadd74608d0b
Marcelo Prates <marceloorp@gmail.com>
2018-03-13 17:56:43 -0300

    AA

--- gender-bias.py
+++ gender-bias.py
@@ -1,10 +1,84 @@
 
 from googletrans import Translator
+from googletrans import LANGUAGES
 from xpinyin import Pinyin
+import csv
+import numpy as np
 
 translator = Translator()
 p = Pinyin()
 
+# Get language list
+languages = []
+with open('languages.csv', 'r') as f:
+    f.readline()
+    for line in f:
+    	language,family,pronomial_gender_system,supported = line.split(";")
+    	if(pronomial_gender_system != 'Yes' and supported.rstrip() == 'Yes'):
+    		languages.append(language)
+#end with
+
+# Read job occupations list into table
+table = np.array(list(csv.reader(open('jobs/bureau_of_labor_statistics_profession_list_gender_filtered_expanded.tsv','r'), delimiter='\t')))
+
+# Compute list of categories
+categories = list(set(table[1:,-3]))
+
+# Get dictionary of category -> jobs (jobs per category)
+categories_dict = dict([ (category, table[table[:,-3] == category,0]) for category in categories ])
+
+"""
+	Create a table with the translated version (provided by Google Translate) of each job,
+	in the following structure:
+		1. Each line corresponds to a single occupation
+		2. The first column is the occupation category ("Computer and mathematical occupations", "Healthcare practitioners and technical occupations", etc.)
+		3. The following N columns give a translated version of that occupation for each language (of N) in our list
+"""
+if False:
+	with open("Results/jobs-translations.tsv","w") as output:
+	
+		# Write header
+		# First column is category
+		output.write("Category")
+		# Then follows one column per language
+		output.write("\tEnglish")
+		for language in languages:
+			output.write('\t' + language)
+		#end for
+		output.write('\n')
+	
+		# Not iterate over all categories
+		for category in categories:
+			print("Translating occupations from category {} ...".format(category))
+			# Get all jobs for this category
+			for job in categories_dict[category]:
+				print("\tTranslating occupation \"{}\" ...".format(job))
+				output.write(category)
+				output.write('\t' + job)
+				# For each language L in our list, translate 'job' from English to L
+				for language in languages:
+					try:
+						if language == 'Chinese':
+							translated_job = (translator.translate(job.rstrip().lower(),src='en',dest='zh-cn').text).lower()
+						else:
+							translated_job = (translator.translate(job.rstrip().lower(),src='en',dest=language).text).lower()
+						#end if
+					except Exception:
+						print("\tCould not translate occupation %s to language %s" % (job.rstrip(),language))
+						translated_job = "?"
+					#end try
+					output.write('\t' + translated_job)
+				#end for
+				output.write('\n')
+			#end for
+		#end for
+	#end with
+#end if
+
+# Get table with one occupation for row, translated to every language (one per column)
+translated_occupations = list(csv.reader(open('Results/jobs-translations.tsv','r'), delimiter='\t'))
+
+# Define function to obtain the translated gender of an occupation in a given language (through Google Translate)
 def get_gender(occupation,language):
 
 	translation = ''
@@ -42,38 +116,48 @@ def get_gender(occupation,language):
 		return 'Neutral'
 	else:
 		return '?'
+#end def
 
-with open('jobs.csv','r') as adjectives:
-	with open('job-genders.csv','w') as output:
+"""
+	Now create
+"""
+discarded_languages = ['Bengali','Nepali','Korean']
+with open('Results/job-genders.tsv','w') as output:
 
-		languages = list(map(lambda x: x.rstrip(), adjectives.readline().split(';')[1:]))
-		discarded_languages = ['Bengali','Nepali','Korean']
+	# Write header
+	output.write("Category")
+	output.write("\tOccupation")
+	for language in languages:
+		if language not in discarded_languages:
+			output.write('\t' + language)
+		#end if
+	#end for
+	output.write('\n')
 
-		# Write header
-		output.write('Category')
-		output.write(';Occupation')
-		for language in languages:
+	for entry in translated_occupations[1:]:
+		
+		category 		= entry[0]
+		english_name 	= entry[1]
+		foreign_names 	= entry[2:]
+
+		print("Translating occupation \"{}\" ...".format(english_name))
+
+		output.write(category)
+		output.write('\t' + english_name)
+
+		for (language, foreign_name) in zip(languages, foreign_names):
 			if language not in discarded_languages:
-				output.write(';%s' % language)
+				try:
+					gender = get_gender(foreign_name,language)
+					output.write('\t%s' % gender)
+				except ValueError:
+					output.write('\t?')
+				#end try
+			#end for
+		#end for
+
 		output.write('\n')
-	
-		# For each job, translate into each language and fetch the corresponding gender
-		for line in adjectives:
-			category = line.split(';')[0]
-			occupation = line.split(';')[1]
-			foreign_names = list(map(lambda x: x.rstrip(), line.split(';')[1:]))
+		output.flush()
+	#end for
 
-			output.write('%s' % category)
-			output.write(';%s' % occupation)
-
-			output.write('%s' % english_name)
-	
-			for (language,foreign_name) in zip(languages,foreign_names):
-				if(language not in discarded_languages):
-					try:
-						gender = get_gender(foreign_name,language)
-						output.write(';%s' % gender)
-					except ValueError:
-						output.write(';?')
-
-			output.write('\n')
+#end with
--- stats.jl
+++ stats.jl
@@ -2,7 +2,87 @@
 using DataFrames, CSV, Gadfly
 
 # Read job-genders.csv into a Julia DataFrame
-dat_jobs = CSV.read("job-genders.csv",delim=';',nullable=false)
+dat_jobs = CSV.read("Results/job-genders.tsv",delim='\t',nullable=false)
+
+# Remove "occupations" from the end of all category names
+dat_jobs[:Category] = map(x -> replace(x," occupations",""),dat_jobs[:Category])
+
+function rename_category(label)
+    if label=="Education, training, and library"
+        return "Education / Training / Library"
+    elseif label=="Business and financial operations"
+        return "Business / Financial"
+    elseif label=="Office and administrative support"
+        return "Office / Administrative"
+    elseif label=="Installation, maintenance, and repair"
+        return "Installation / Maintenance / Repair"
+    elseif label=="Healthcare practitioners and technical"
+        return "Healthcare practitioners / technical"
+    elseif label=="Life, physical, and social science"
+        return "Life, physical, and social sciences"
+    elseif label=="Arts, design, entertainment, sports, and media"
+        return "Arts / Entertainment"
+    elseif label=="Building and grounds cleaning and maintenance"
+        return "Cleaning / Maintenance"
+    elseif label=="Food preparation and serving related"
+        return "Food prepaparation / serving"
+    elseif label=="Transportation and material moving"
+        return "Transportation / material moving"
+    else
+        return label
+    end
+end
+
+function group_categories(label)
+    if label == "Education, training, and library"
+        return "Education"
+    elseif label == "Business and financial operations"
+        return "Corporate"
+    elseif label == "Office and administrative support"
+        return "Service"
+    elseif label == "Healthcare support"
+        return "Healthcare"
+    elseif label == "Management"
+        return "Corporate"
+    elseif label == "Installation, maintenance, and repair"
+        return "Service"
+    elseif label == "Healthcare practitioners and technical"
+        return "Healthcare"
+    elseif label == "Community and social service"
+        return "Service"
+    elseif label == "Sales and related"
+        return "Corporate"
+    elseif label == "Production"
+        return "Production"
+    elseif label == "Architecture and engineering"
+        return "STEM"
+    elseif label == "Life, physical, and social science"
+        return "STEM"
+    elseif label == "Transportation and material moving"
+        return "Service"
+    elseif label == "Arts, design, entertainment, sports, and media"
+        return "Arts \n Entertainment"
+    elseif label == "Legal"
+        return "Legal"
+    elseif label == "Protective service"
+        return "Service"
+    elseif label == "Food preparation and serving related"
+        return "Service"
+    elseif label == "Farming, fishing, and forestry"
+        return "Farming \n Fishing \n Forestry"
+    elseif label == "Computer and mathematical"
+        return "STEM"
+    elseif label == "Personal care and service"
+        return "Service"
+    elseif label == "Construction and extraction"
+        return "Construction \n Extraction"
+    elseif label == "Building and grounds cleaning and maintenance"
+        return "Service"
+    end
+end
+
+dat_jobs[:Category] = map(x -> rename_category(x), dat_jobs[:Category])
+
 # Read adjective-genders.csv into a Julia DataFrame
 dat_adjectives = CSV.read("adjective-genders.csv",delim=';',nullable=false)
 
@@ -27,7 +107,7 @@ function barplot_adjectives()
 
     # Save bar plot
     p = plot(results_by_adjective, x=:Adjective, y=:Count, color=:Gender, Coord.Cartesian(ymin=0,ymax=100), Geom.bar(position=:stack), Theme(background_color="white",grid_color="gray",bar_highlight=colorant"black"), Guide.ylabel("\%"))
-    draw(PDF("paper/pictures/barplot-adjectives.pdf", 5inch, 3.75inch),p)
+    draw(PDF("Paper/pictures/barplot-adjectives.pdf", 5inch, 3.75inch),p)
 end
 
 function histograms_occupations()
@@ -46,14 +126,14 @@ function histograms_occupations()
     end
 
     # Save histograms
-    p = plot(results_by_occupation, x=:Female, color=:Category, Guide.xlabel("\# Female Pronouns"), Geom.histogram)
-    draw(PDF("paper/pictures/histogram-female.pdf", 5inch, 3.75inch),p)
+    p = plot(results_by_occupation, x=:Female, color=:Category, Guide.xlabel("\# Female Pronouns"), Geom.histogram, Theme(key_max_columns=1, key_label_font_size=6pt))
+    draw(PDF("Paper/pictures/histogram-female.pdf", 5.0inch, 3.75inch),p)
 
-    p = plot(results_by_occupation, x=:Male, color=:Category, Guide.xlabel("\# Male Pronouns"), Geom.histogram)
-    draw(PDF("paper/pictures/histogram-male.pdf", 5inch, 3.75inch),p)
+    p = plot(results_by_occupation, x=:Male, color=:Category, Guide.xlabel("\# Male Pronouns"), Geom.histogram, Theme(key_max_columns=1, key_label_font_size=6pt))
+    draw(PDF("Paper/pictures/histogram-male.pdf", 5.0inch, 3.75inch),p)
 
-    p = plot(results_by_occupation, x=:Neutral, color=:Category, Guide.xlabel("\# Gender Neutral Pronouns"), Geom.histogram)
-    draw(PDF("paper/pictures/histogram-neutral.pdf", 5inch, 3.75inch),p)
+    p = plot(results_by_occupation, x=:Neutral, color=:Category, Guide.xlabel("\# Gender Neutral Pronouns"), Geom.histogram, Theme(key_max_columns=1, key_label_font_size=6pt))
+    draw(PDF("Paper/pictures/histogram-neutral.pdf", 5.0inch, 3.75inch),p)
 end
 
 function barplots_category()
@@ -70,14 +150,16 @@ function barplots_category()
         neutral_count   = sum([ count(x -> x=="Neutral",    aux[language]) for language in names(aux) ])
         total           = sum([ count(x -> true,            aux[language]) for language in names(aux) ])
 
-        push!(results_by_category,[ucfirst(category),"Female",  100*female_count/total  ])
-        push!(results_by_category,[ucfirst(category),"Male",    100*male_count/total    ])
-        push!(results_by_category,[ucfirst(category),"Neutral", 100*neutral_count/total ])
+        push!(results_by_category,[ucfirst(category),"Female",  100 * female_count    / total ])
+        push!(results_by_category,[ucfirst(category),"Male",    100 * male_count      / total ])
+        push!(results_by_category,[ucfirst(category),"Neutral", 100 * neutral_count   / total ])
     end
+
+    CSV.write("Results/results-by-category.dat",results_by_category)
 
     # Save bar plot
     p = plot(results_by_category, x="Category", y="Count", color="Gender", Coord.Cartesian(ymin=0,ymax=100), Geom.bar(position=:stack), Theme(background_color="white",grid_color="gray",bar_highlight=colorant"black"), Guide.ylabel("\%"))
-    draw(PDF("paper/pictures/gender-by-category.pdf", 5inch, 3.75inch),p)
+    draw(PDF("Paper/pictures/gender-by-category.pdf", 5inch, 3.75inch),p)
 end
 
 function barplots_language()
@@ -90,17 +172,20 @@ function barplots_language()
         male_count      = count(x -> x=="Male",     dat_jobs[language])
         neutral_count   = count(x -> x=="Neutral",  dat_jobs[language])
         total           = count(x -> true,          dat_jobs[language])
-        
-        push!(results_by_language,[language,"Female",   100*female_count/total    ])
-        push!(results_by_language,[language,"Male",     100*male_count/total      ])
-        push!(results_by_language,[language,"Neutral",  100*neutral_count/total   ])
+
+        push!(results_by_language,[language,"Female",   100 * female_count    / total  ])
+        push!(results_by_language,[language,"Male",     100 * male_count      / total  ])
+        push!(results_by_language,[language,"Neutral",  100 * neutral_count   / total  ])
     end
+
+    CSV.write("Results/results-by-language.dat",results_by_language)
+
     # Save bar plot
     p = plot(results_by_language, x="Language", y="Count", color="Gender", Coord.Cartesian(ymin=0,ymax=100), Geom.bar(position=:stack), Theme(background_color="white",grid_color="gray",bar_highlight=colorant"black"), Guide.ylabel("\%"))
-    draw(PDF("paper/pictures/gender-by-language.pdf", 5inch, 3.75inch),p)
+    draw(PDF("Paper/pictures/gender-by-language.pdf", 5inch, 3.75inch),p)
 end
 
-barplot_adjectives()
-histograms_occupations()
-barplots_category()
-barplots_language()
+#barplot_adjectives()
+#histograms_occupations()
+#barplots_category()
+#barplots_language()
